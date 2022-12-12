1967c9dd171c73ea0cc57a31980de6680c11c1a3
KoHtet <kokoh6329@gmail.com>
2022-12-12 22:49:21 +0630

    add lazy

--- src/App.js
+++ src/App.js
@@ -1,7 +1,12 @@
-import { Login, Home, Welcome, Signup } from "./pages";
 import { ProtectRoute } from "./components";
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import UserAuthContextProvider from "./context/userAuthContext";
+import { lazy } from "react";
+
+const Login = lazy(() => import("./pages/Login"));
+const Home = lazy(() => import("./pages/Home"));
+const Welcome = lazy(() => import("./pages/Welcome"));
+const Signup = lazy(() => import("./pages/Signup"));
 
 function App() {
   return (
